da5a74ae34a3df08f875e05b9db29caf5269b4d7
Harmanpreet Singh <me.hpsingh01@gmail.com>
2023-05-12 10:20:19 +0530

    env variables

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("process.env.MONGO_URI", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
     console.log("MongoDB Connected...");
